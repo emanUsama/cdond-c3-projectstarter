1c27e02bfe14ffe35a445c76ebdc1b4c0da3707e
emanUsama <emanusama269@gmail.com>
2022-11-30 10:56:30 +0200

    smoke-test

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_HOST=database-2.cisfernnpcng.us-east-1.rds.amazonaws.com
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=4JByufpm5rmy0sqyA8gf
+export TYPEORM_DATABASE=glee
+export TYPEORM_PORT=5432
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_MIGRATIONS_DIR=./migrations
